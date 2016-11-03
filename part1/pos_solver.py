b5622ef425100efc715f6f607c66f62c5d733f06
Pratik <jainps@iu.edu>
2016-11-03 11:57:36 -0400

    Added code for replacing 0 by 1

--- part1/pos_solver.py
+++ part1/pos_solver.py
@@ -19,9 +19,12 @@ import math
 # that we've supplied.
 #
 class Solver:
-
     # Calculate the log of the posterior probability of a given sentence
     #  with a given part-of-speech labeling
+    def __init__(self):
+        # Initialising the parts of speech dictionary
+        self.POS = ["det", ".", "x", "noun", "verb", "prt", "pron", "num", "adp", "adv", "pron", "adj", "conj"]
+
     def posterior(self, sentence, label):
         return 0
 
@@ -30,26 +33,25 @@ class Solver:
     def train(self, data):
         # Calculating P(S1)
 
-        #Initailizing the parts of speech array
-        self.POS = ["det", ".", "x", "noun", "verb", "prt", "pron", "num", "adp", "adv", "pron", "adj", "conj"]
-        #Initiailizing the parts of speech dictionary    
         s1 = {}
         for partsOfSpeech in self.POS:
             s1.update({partsOfSpeech: 0})
         print s1
-        
-        #Counting the number of times a particular part of speech starts with a sentence
+
+        # Counting the number of times a particular part of speech starts with a sentence
         for sentence in data:
             s1[sentence[1][0]] += 1
         print s1
 
-        #Counting the probability for each part of speech to start in a sentence
-        size_of_data = sum(s1.values())
+        # Check if sentence does not start from a part of speech and set it to 1
         for key in s1.keys():
             if s1[key] == 0:
-                s1[key] = (1.0) / (size_of_data)
-            else:
-                s1[key] = (s1[key] * 1.0) / (size_of_data * 1.0)
+                s1[key] = 1.0
+
+        # Counting the probability for each part of speech to start in a sentence
+        size_of_data = sum(s1.values())
+        for key in s1.keys():
+            s1[key] = (s1[key] * 1.0) / (size_of_data * 1.0)
 
         self.S1 = s1
 
@@ -58,21 +60,21 @@ class Solver:
         for key in s1.keys():
             s[key] = {}
         for sentence in data:
-            for i in range(1,len(sentence[1])):
-                if sentence[1][i] in s[sentence[1][i-1]]:
+            for i in range(1, len(sentence[1])):
+                if sentence[1][i] in s[sentence[1][i - 1]]:
                     s[sentence[1][i - 1]][sentence[1][i]] += 1
                 else:
-                    s[sentence[1][i - 1]].update({sentence[1][i]:1})
+                    s[sentence[1][i - 1]].update({sentence[1][i]: 1})
         for key in s.keys():
             for inner_key in s.keys():
                 if inner_key not in s[key]:
-                    s[key].update({inner_key:1})
+                    s[key].update({inner_key: 1})
         for key in s.keys():
             key_sum = sum(s[key].values())
             for inner_key in s.keys():
-                s[key][inner_key] = (s[key][inner_key]*1.0) / (key_sum)
-
+                s[key][inner_key] = (s[key][inner_key] * 1.0) / key_sum
         self.transitionProbabilities = s
+
         # Calculating P(Wi|Si)
         w_s = {}
         for partsOfSpeech in self.POS:
@@ -83,25 +85,25 @@ class Solver:
                 if sentence[0][i] in w_s[sentence[1][i]]:
                     w_s[sentence[1][i]][sentence[0][i]] += 1
                 else:
-                    w_s[sentence[1][i]].update({ sentence[0][i] : 1 })
+                    w_s[sentence[1][i]].update({sentence[0][i]: 1})
 
         for key in w_s.keys():
             key_sum = sum(w_s[key].values())
             for inner_key in w_s[key].keys():
-                w_s[key][inner_key] = (w_s[key][inner_key] * 1.0) / (key_sum)
+                w_s[key][inner_key] = (w_s[key][inner_key] * 1.0) / key_sum
 
         self.emissionProbabilities = w_s
+
     # Functions for each algorithm.
     #
     def simplified(self, sentence):
-        return [ [ [ "noun" ] * len(sentence)], [[0] * len(sentence),] ]
+        return [[["noun"] * len(sentence)], [[0] * len(sentence), ]]
 
     def hmm(self, sentence):
-        return [ [ [ "noun" ] * len(sentence)], [] ]
+        return [[["noun"] * len(sentence)], []]
 
     def complex(self, sentence):
-        return [ [ [ "noun" ] * len(sentence)], [[0] * len(sentence),] ]
-
+        return [[["noun"] * len(sentence)], [[0] * len(sentence), ]]
 
     # This solve() method is called by label.py, so you should keep the interface the
     #  same, but you can change the code itself. 
@@ -122,4 +124,3 @@ class Solver:
             return self.complex(sentence)
         else:
             print "Unknown algo!"
-
